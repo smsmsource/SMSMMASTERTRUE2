09f82d4cf98221054f593267ff255f995c06d4f5
smsmsource <100517954+smsmsource@users.noreply.github.com>
2022-06-15 03:48:19 +0200

    Update inline.py

--- program/utils/inline.py
+++ program/utils/inline.py
@@ -1,40 +1,34 @@
 """ inline section button """
 
-from config import BOT_USERNAME
 from pyrogram.types import (
-  CallbackQuery,
   InlineKeyboardButton,
   InlineKeyboardMarkup,
-  Message,
 )
 
 
 def stream_markup(user_id):
   buttons = [
     [
-      InlineKeyboardButton(text="𝗠𝗲𝗻𝘂 🖱️", callback_data=f'stream_menu_panel | {user_id}'),
-      InlineKeyboardButton(text="𝗖𝗹𝗼𝘀𝗲 🗑️", callback_data=f'set_close'),
+      InlineKeyboardButton(text="• Mᴇɴᴜ", callback_data=f'stream_menu_panel | {user_id}'),
+      InlineKeyboardButton(text="• Cʟᴏsᴇ", callback_data=f'set_close'),
     ],
-    [
-      InlineKeyboardButton("[⌯ 𝗗͛𝗲͛𝘃͛ 𝗦͛𝗺͛𝗦͛𝗺͛ 𝗘͛𝗹͛𝗞͛𝗯͛𝗘͛𝗿͛ - ⌯]𝟏𝟎𝐊🍷𖣩ًََِْٰٓ ⃝⃙🇽🇰 ⌯", callback_data="smsmelkbeer")
   ]
- ]
   return buttons
 
 
 def menu_markup(user_id):
   buttons = [
     [
-      InlineKeyboardButton(text="𝗘𝗡𝗗 ⏹", callback_data=f'set_stop | {user_id}'),
-      InlineKeyboardButton(text="𝗣𝗔𝗨𝗦𝗘 ⏸", callback_data=f'set_pause | {user_id}'),
-      InlineKeyboardButton(text="𝗥𝗘𝗦𝗨𝗠𝗘 ▶️", callback_data=f'set_resume | {user_id}'),
+      InlineKeyboardButton(text="⏹", callback_data=f'set_stop | {user_id}'),
+      InlineKeyboardButton(text="⏸", callback_data=f'set_pause | {user_id}'),
+      InlineKeyboardButton(text="▶️", callback_data=f'set_resume | {user_id}'),
     ],
     [
-      InlineKeyboardButton(text="𝗠𝗨𝗧𝗘 🔇", callback_data=f'set_mute | {user_id}'),
-      InlineKeyboardButton(text="𝗨𝗡 𝗠𝗨𝗧𝗘 🔊", callback_data=f'set_unmute | {user_id}'),
+      InlineKeyboardButton(text="🔇", callback_data=f'set_mute | {user_id}'),
+      InlineKeyboardButton(text="🔊", callback_data=f'set_unmute | {user_id}'),
     ],
     [
-      InlineKeyboardButton(text="[⌯ 𝗗͛𝗲͛𝘃͛ 𝗦͛𝗺͛𝗦͛𝗺͛ 𝗘͛𝗹͛𝗞͛𝗯͛𝗘͛𝗿͛ - ⌯]𝟏𝟎𝐊🍷𖣩ًََِْٰٓ ⃝⃙🇽🇰 ⌯", callback_data='stream_home_panel'),
+      InlineKeyboardButton(text="🔙 Go Back", callback_data='stream_home_panel'),
     ]
   ]
   return buttons
@@ -44,7 +38,7 @@ close_mark = InlineKeyboardMarkup(
   [
     [
       InlineKeyboardButton(
-        "𝗖𝗹𝗼𝘀𝗲 🗑️", callback_data="set_close"
+        "🗑 Close", callback_data="set_close"
       )
     ]
   ]
@@ -55,7 +49,7 @@ back_mark = InlineKeyboardMarkup(
   [
     [
       InlineKeyboardButton(
-        "🔙 𝗚𝗼 𝗕𝗮𝗰𝗸", callback_data="stream_menu_panel"
+        "🔙 Go Back", callback_data="stream_menu_panel"
       )
     ]
   ]
